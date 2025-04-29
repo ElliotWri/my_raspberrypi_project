01024dbaa3059b924d41b91bea3b460301caec83
ElliotWri <wrightelliot106@gmail.com>
2025-04-29 16:18:01 -0500

    adds buzzer to pir

--- python/ch_13_pir.py
+++ python/ch_13_pir.py
@@ -1,7 +1,13 @@
-from gpiozero import MotionSensor
-
-pir = MotionSensor(18)
+from gpiozero import MotionSensor,Buzzer
+buzzer= Buzzer(18)
+def buzz(pitch, duration):
+    period = 1.0 / pitch
+    delay = period / 2
+    cycles = int(duration * pitch)
+    buzzer.beep(on_time=period, off_time=period, n=int(cycles/2))
+pir = MotionSensor(23)
 
 while True:
     pir.wait_for_motion()
     print("Motion detected!")
+    buzz(100,1)
